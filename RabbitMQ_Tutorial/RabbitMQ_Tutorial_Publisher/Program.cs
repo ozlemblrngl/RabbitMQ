d599c681fc063cbb7ccb8f1e9ae4c6dd10fcc8cc
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-28 21:26:06 +0300

    fanout publisher and subscriber added

--- RabbitMQ_Tutorial/RabbitMQ_Tutorial_Publisher/Program.cs
+++ RabbitMQ_Tutorial/RabbitMQ_Tutorial_Publisher/Program.cs
@@ -5,55 +5,23 @@ internal class Program
 {
 	private static void Main(string[] args)
 
-	//bir kere bağlantı oluşturduğumuzda birden fazla kanal oluşturabiliriz. Çünkü bağlantı oluşturmak maliyetli bir şeydir
-	//ve bir bağlantıda birden fazla kanalın kullanılması sağlanır.
 	{
-		// uri aşağıda yazmak best practice değil, appsetting de yazmamız lazım normalde.
 
 		var factory = new ConnectionFactory();
 		factory.Uri = new Uri("amqps://otbrrpua:CjXwGN5SwWcSupcScy7r0HIM1xo2915_@woodpecker.rmq.cloudamqp.com/otbrrpua");
 
-		//using (var connection = factory.CreateConnection())
-		//{
-
-		//} olarak da kullanılabilir.
-		//Aradaki fark aşağıdakinin bu sefer süslü parantezi maine kadar genişletmiş olmasıdır.
-
 		using var connection = factory.CreateConnection();
-
-		// aşağıda oluşturduğumuz kanal üzerinden rabbitmq ile haberleşebiliriz. 
 		var channel = connection.CreateModel();
 
-		// oluşturulan kanal üzerinden mesajların boşa düşmemesi için bir kuyruk (queue) oluşturmamız lazım.
-		// aşağıda kuyruk oluşturuyoruz
-		// bool durable parametresini true yapmamız lazım.
-		// yapmazsak mesajlar in memory de tutulur ve yeniden başlama durumunda hepsi yok olur.
-		// True olduğunda ise kuyruklar fiziksel olarak kaydedilir ve restart durumunda kaybolmaz.
-		// bool exclusive 'i true yaparsak farklı kanallardan rabbitmq ya ulaşamayız ama biz burada farklı process le rabbitmq'ya
-		// farklı kanallardan da ulaşmak istediğimizden false yapıyoruz 3. parametreyi.
-		// bool autodelete te de bu kuyruğa bağlı olan subscriber bağlantısını kopartırsa kuyruk da silinir demek.
-		// bu bizim isteyeceğimiz bir durum değil keza subsriber yanlışlıkla down olursa kuyruk da silinir.
-		// Biz istiyoruz ki kuyruk her zaman ayakta dursun. O nedenle otomatik silmeyi kapatıyoruz, false a çekiyoruz.
-		// ama subscriberların hepsi yani sonuncusu dştüğünde artık kuyruğumuz silinsin diyorsak true da yapabiliriz.
-
-		channel.QueueDeclare("hello-queue", true, false, false);
-
-		// rabbitmq ya mesajlarımızı bir byte dizimi olarak göndeririz ve bu büyük bir avantajdır. pdf de gönderebiliriz. İmage da gönderebiliriz.
-		// istediğimiz her şeyi gönderebiliriz.
-		// bir publisher birden fazla subscribera mesaj gönderebilir 
-
+		channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout); // durable yapmazsak uygulama restrat olduğunda exchange silinir.
 
 		Enumerable.Range(1, 50).ToList().ForEach(x =>
 		{
-			string message = $"mesajımız {x}";
+			string message = $"log {x}";
 
 			var messageBody = Encoding.UTF8.GetBytes(message);
 
-			// aşağıdaki metodun extension unda echange var ancak biz şu an exchange kullanmayacağız.
-			// exchange kullanılmayan hali de default exchange olarak geçer. 
-			// bu durumda routingkey'e kuruğun adını vermemiz gerekir. bkz. hello-queue
-
-			channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+			channel.BasicPublish("logs-fanout", "", null, messageBody);
 
 
 			Console.WriteLine($"mesaj gönderilmiştir {message}");
--- RabbitMQ_Tutorial/RabbitMQ_Tutorial_Subscriber/Program.cs
+++ RabbitMQ_Tutorial/RabbitMQ_Tutorial_Subscriber/Program.cs
@@ -12,26 +12,24 @@ internal class Program
 		using var connection = factory.CreateConnection();
 		var channel = connection.CreateModel();
 
-		// kuyruk oluşturma işlemini ister publisher ister subscriber kısmında yapabiliriz.
-		// eğer publisherda kuyruğun oluştuğundan eminsek buradaki kodu silebiliriz ancak değilsek bu durumda hata alırız.
-		// publisherda yoksa burada kuyruğun adının olması lazım.Yani aşağıdaki kodun.
-		// hem publisher da hem subscriberda olması da hataya neden olmaz.Ancak parametreleri aynı olmalı.
-		// channel.QueueDeclare("hello-queue", true, false, false);
-
-		// her bir subscriber a bir tane mesaj gelsin diyoruz. 
-		//bool global de ise false olursa örneğin (0,6,false) oldu bu durumda her bir subscriber a 6 mesaj gider
-		// true olursa (0,6,true) bu durumda 3 subsriber varsa her birine 2 şer mesaj gider, 2 subscriber varsa her birine 3 mesaj gider.
-		// aşağıdaki örnekte true da desek 1 mesajı bölemeyeceği için false gibi işlem yapar ve her bir subscriber a 1 mesaj gönderir.
+		var randomQueueName = channel.QueueDeclare().QueueName; // random kuyrukismi oluşturuyoruz.
+																//channel.QueueDeclare(randomQueueName,true,false,false);   // eğer bunu yazsaydık ilgili subscriber(instance) kapansa dahi kuyruk kalırdı. Bu kuyruğun kalmasını istemediğimiz için aşağıdaki kodu yazdık
+		channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+		// burada yeni bir kuyruk oluşturmuyoruz kanal üzerinden kuyruğu bind ediyoruz.Burada subscriber bağlantısını kestiğinde kuyruk da silinir. O şekilde ayarlıyoruz burada.
+		// o nedenle bir kuyruk oluşturmak yerine channel üzerinden bir kuyruk bind ediyoruz.
+		// artık uygulama her ayağa kalktığında bir kuyruk bind olacak ama down olduğunda ilgili kuyruk silinecek.
 
 		channel.BasicQos(0, 1, false);
 
 		var consumer = new EventingBasicConsumer(channel);
 
-		//bool autoAck true ise bunun anlamı rabbitmq subcriber a mesaj gönderdiğinde bunu doğru da işlese yanlış da işlese siler.
-		//false ise rabbitmq mesajı ilettiğinde sen bunu direkt silme bunu doğru işlesin işledikten sonra ben sana haber edeceğim.
-		// haber verme kodu da devamında  
+		channel.BasicConsume(randomQueueName, false, consumer);
 
-		channel.BasicConsume("hello-queue", false, consumer);
+		Console.WriteLine("Loglar Dinleniyor.");
+
+
+		// channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout); 
+		// artık bunu yazmamıza gerek yok, publisher exchange i oluşturdu kod diğer tarafta var, yazsak da sorun olmaz  sadece ayarları değişik yaparsak sorun olur
 
 		consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 		{
@@ -40,10 +38,6 @@ internal class Program
 			Thread.Sleep(1500);
 
 			Console.WriteLine("Gelen Mesaj : " + message);
-
-			// ulaştırılan tag'ı rabbitmq ya gönderiyoruz ve ulaşan mesajı raabitmq kuyruktan siliyor.
-			// diğer değeri true girersek o sırada memoryde işlenmiş ama rabbitmq ya gitmemişsse mesajlar onun bilgilerini de rabbitmq ya haberdar etmek için.
-			// biz şimdilik false giriyoruz. Böylece sadece ilgili mesajın durumunu raabitmq ya bildir diyoruz. 
 
 			channel.BasicAck(e.DeliveryTag, false);
 		};
