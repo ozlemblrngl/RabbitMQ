0f753d7b78bc49b3d2808beffca70024a4ca0a05
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-10-03 14:36:21 +0300

    because of ImageName attribute dint come null, adding nullable sign of ? to property solved the problem

--- WatermarkApp_RabbitMQ/WatermarkApp_RabbitMQ/Models/Product.cs
+++ WatermarkApp_RabbitMQ/WatermarkApp_RabbitMQ/Models/Product.cs
@@ -3,21 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WatermarkApp_RabbitMQ.Models
 {
-	public class Product
-	{
-		[Key]
-		public int Id { get; set; }
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
 
-		[StringLength(100)]
-		public string Name { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
 
-		[Column(TypeName = "decimal(18,2")] // 16 karakteri virgülden önce 2 karakteri virgülden sonra
-		public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2")] // 16 karakteri virgülden önce 2 karakteri virgülden sonra
+        public decimal Price { get; set; }
 
-		[Range(1, 100)]
-		public int Stock { get; set; }
+        [Range(1, 100)]
+        public int Stock { get; set; }
 
-		[StringLength(100)]
-		public string ImageName { get; set; }
-	}
+        [StringLength(100)]
+        public string? ImageName { get; set; }
+    }
 }
